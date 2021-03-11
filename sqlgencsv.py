58aa3ee2da90b8cab5935790fb43d317def49c26
mycelo1 <mycelo@gmail.com>
2021-03-11 11:01:00 -0300

    line number, fixed value

--- sqlgencsv.py
+++ sqlgencsv.py
@@ -4,6 +4,12 @@ import argparse
 import csv
 import sys
 
+def tryFloat(number_string):
+    try:
+        return float(number_string)
+    except ValueError:
+        pass
+
 def f_arg_normalize(arg):
     if len(arg) > 1:
         return arg[1:2]
@@ -11,11 +17,13 @@ def f_arg_normalize(arg):
         return arg
 
 arg_parser = argparse.ArgumentParser()
-arg_parser.add_argument('-t', type = ascii, required = 'true', dest = 'table_name', help = 'table name')
-arg_parser.add_argument('-d', type = ascii, default = ',', dest = 'delimiter', help = 'delimiter char')
-arg_parser.add_argument('-q', type = ascii, default = '"', dest = 'quotechar', help = 'quotation char')
+arg_parser.add_argument('-t', type = ascii, required = True, dest = 'table_name', help = 'table name')
 arg_parser.add_argument('-c', type = int, dest = 'column_number', nargs = '*', help = 'list of columns to output')
-arg_parser.add_argument('-v', type = ascii, dest = 'first_value', help = 'optional fixed value for first column')
+arg_parser.add_argument('-d', type = ascii, default = ',', dest = 'delimiter', help = 'CSV delimiter char')
+arg_parser.add_argument('-q', type = ascii, default = '"', dest = 'quotechar', help = 'CSV quote char')
+arg_parser.add_argument('-l', action = 'store_true', dest = 'line_number', default = False, help = 'add line number as first column')
+arg_parser.add_argument('-v', type = ascii, dest = 'fixed_value', help = 'add fixed value as last column')
+arg_parser.add_argument('-n', action = 'store_true', dest = 'detect_number', default = False, help = 'supress quotes for values detected as numeric')
 arguments = arg_parser.parse_args()
 
 delimiter = f_arg_normalize(arguments.delimiter)
@@ -26,18 +34,29 @@ csv_reader = csv.reader(
     delimiter = delimiter,
     quotechar = quotechar)
 
+line_number = 0
+
 for csv_row in csv_reader:
-    if arguments.first_value is None:
-        sql_line = ''
+    line_number += 1
+    if arguments.line_number:
+        sql_line = '{}'.format(line_number)
     else:
-        sql_line = '"' + arguments.first_value.strip('"\'') + '"'
+        sql_line = ''
     for csv_column in range(0, len(csv_row)):
         if (arguments.column_number is None) or ((csv_column + 1) in arguments.column_number):
             column_value = csv_row[csv_column].strip().replace('"', '\\"')
             if len(sql_line) > 0:
                 sql_line = sql_line + ', '            
             if len(column_value) > 0:
-                sql_line = sql_line + '"' + column_value + '"'
+                number_parse = tryFloat(column_value)
+                if (arguments.detect_number) and (number_parse is not None):
+                    sql_line = sql_line + column_value
+                else:
+                    sql_line = sql_line + '"' + column_value + '"'
             else:
                 sql_line = sql_line + 'NULL'
+    if arguments.fixed_value is not None:
+        if len(sql_line) > 0:
+            sql_line = sql_line + ', '
+        sql_line = sql_line + '"' + arguments.fixed_value.strip('"\'') + '"'
     print('INSERT INTO {} VALUES ({});'.format(arguments.table_name.strip('\'"'), sql_line))
--- sqlgentxt.py
+++ sqlgentxt.py
@@ -4,11 +4,19 @@ import argparse
 import re
 import sys
 
+def tryFloat(number_string):
+    try:
+        return float(number_string)
+    except ValueError:
+        pass
+
 arg_parser = argparse.ArgumentParser()
-arg_parser.add_argument('-t', type = ascii, required = 'true', dest = 'table_name', help = 'table name')
-arg_parser.add_argument('-c', type = ascii, required = 'true', dest = 'column_range', nargs = '+', help = 'list of columns to output (p1-p2)')
-arg_parser.add_argument('-v', type = ascii, dest = 'first_value', help = 'optional fixed value for first column')
+arg_parser.add_argument('-t', type = ascii, required = True, dest = 'table_name', help = 'table name')
+arg_parser.add_argument('-c', type = ascii, required = True, dest = 'column_range', nargs = '+', help = 'list of columns to output (p1-p2)')
 arg_parser.add_argument('-m', type = ascii, dest = 'regexp', help = 'only lines matching this regular expression')
+arg_parser.add_argument('-l', action = 'store_true', dest = 'line_number', default = False, help = 'add line number as first column')
+arg_parser.add_argument('-v', type = ascii, dest = 'fixed_value', help = 'add fixed value as last column')
+arg_parser.add_argument('-n', action = 'store_true', dest = 'detect_number', default = False, help = 'supress quotes for values detected as numeric')
 arguments = arg_parser.parse_args()
 
 if arguments.regexp is None:
@@ -17,12 +25,15 @@ else:
     is_regexp = True
     regexp = re.compile(arguments.regexp.strip('"\''))
     
+line_number = 0
+
 for line in sys.stdin:
+    line_number += 1
     if (not is_regexp) or (regexp.search(line)):
-        if arguments.first_value is None:
-            sql_line = ''
+        if arguments.line_number:
+            sql_line = '{}'.format(line_number)
         else:
-            sql_line = '"' + arguments.first_value.strip('"\'') + '"'
+            sql_line = ''
         for arg_column in arguments.column_range:
             arg_column_split = arg_column.strip('"\'').split('-')
             column_start = int(arg_column_split[0])
@@ -31,7 +42,15 @@ for line in sys.stdin:
             if len(sql_line) > 0:
                 sql_line = sql_line + ', '
             if len(column_value) > 0:
-                sql_line = sql_line + '"' + column_value + '"'
+                number_parse = tryFloat(column_value)
+                if (arguments.detect_number) and (number_parse is not None):
+                    sql_line = sql_line + column_value
+                else:
+                    sql_line = sql_line + '"' + column_value + '"'
             else:
                 sql_line = sql_line + 'NULL'
+        if arguments.fixed_value is not None:
+            if len(sql_line) > 0:
+                sql_line = sql_line + ', '
+            sql_line = sql_line + '"' + arguments.fixed_value.strip('"\'') + '"'
         print('INSERT INTO {} VALUES ({});'.format(arguments.table_name.strip('"\''), sql_line))
